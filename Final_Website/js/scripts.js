a9aa47ea38eb376b8fcc5b2de10f826a7f7ed799
saunders-jake <59486347+saunders-jake@users.noreply.github.com>
2021-12-02 00:14:34 -0800

    Delete Final_Website directory

--- Final_Website/js/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $("#datepicker1").datepicker({
-        todayHighlight: true, 
-        format: 'yyyy-mm-dd', 
-        startDate: new Date(), 
-        endDate: new Date('2022-5-31'),
-        toggleActive: true,
-    });     
-});
